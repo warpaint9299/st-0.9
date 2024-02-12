bc2e3d513f6751a6c91df4995c55c7247a3a5120
warpaint <warpaint9299@gmail.com>
2024-02-12 21:44:49 +0800

    update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VictorMono Nerd Font:style=Bold:pixelsize=23:antialias=true:autohint=true";
+static char *font = "VictorMono Nerd Font:style=Bold:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
