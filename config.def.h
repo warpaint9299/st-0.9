30838fb9474b169b2facdd4cbbdf71449d8d5985
warpaint <warpaint9299@gmail.com>
2024-03-17 00:34:55 +0800

    update default font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "VictorMono Nerd Font:style=Bold:pixelsize=22:antialias=true:autohint=true";
+static char *font = "VictorMono Nerd Font:style=Bold:pixelsize=18:antialias=true:autohint=true";
 
 /* Spare fonts */ 
 static char *font2[] = {
